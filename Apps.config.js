dfc65866e8389171f261897dad5f95560277e5ab
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-18 21:04:02 +0530

    Initial commit
    Configurations for Build/Test/Release [BTR] of Apps®, http://apps.sequomics.com/.

---
+++ Apps.config.js
@@ -0,0 +1,12 @@
+/*!
+ * Apps® – [v1.0.0]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 18-April-2016
+ * @license   : Apache, version 2.0
+ * ______________________________________________________________
+ */

5842b5ee9613de76ce79556c8bce0a9646a1e1bc
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 00:40:30 +0530

    Initial commit
    Path: config/tasks/comb.js
    grunt-csscomb: The grunt plugin for sorting CSS properties in specific order.
    Usage: $ npm install grunt-csscomb --save-dev

---
+++ config/tasks/comb.js
@@ -0,0 +1,22 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~
+ * grunt-csscomb  : https://github.com/csscomb/grunt-csscomb
+ * Help           : http://github.com/csscomb/csscomb.js
+ * Usage          : $ npm install grunt-csscomb --save-dev
+ * @require       : rules/.csscomb.json
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 18-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________
+ */

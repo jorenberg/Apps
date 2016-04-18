80dd490e26a188086faa52ad4db428d3c5aab823
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 02:38:20 +0530

    Initial commit
    Path: config/tasks/remove.js
    grunt-exec: Grunt plugin for executing shell commands, and removing logs/temporaries.
    Usage: $ npm install grunt-exec --save-dev

---
+++ config/tasks/remove.js
@@ -0,0 +1,20 @@
+/*!
+ * Apps®
+ * ______________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~
+ * grunt-exec : https://github.com/jharding/grunt-exec
+ * Usage      : $ npm install grunt-exec --save-dev
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ______________________________________________________________
+ */

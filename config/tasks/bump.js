0cb7a51d1078ac72ede43dba94fbabb4faa58ffa
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-18 23:26:11 +0530

    Initial commit
    Path: config/tasks/bump.js
    Bump: Increment package version.
    Usage: $ npm install grunt-bump --save-dev

---
+++ config/tasks/bump.js
@@ -0,0 +1,22 @@
+/*!
+ * Apps®
+ * __________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~
+ * Bump       :======================================================
+ *  |-------------- Bump package version, create tag, commit, push...
+ * grunt-bump : https://github.com/vojtajina/grunt-bump
+ * Usage      : $ npm install grunt-bump --save-dev
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________
+ * @date      : 18-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * __________________________________________________________________
+ */

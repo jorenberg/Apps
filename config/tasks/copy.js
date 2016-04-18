2c8a12dbd4a84d59b55d4ca680cbea58d50c8495
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 01:01:40 +0530

    Initial commit
    Path: config/tasks/copy.js
    Usage: $ npm install grunt-contrib-copy --save-dev

---
+++ config/tasks/copy.js
@@ -0,0 +1,20 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-copy : https://github.com/gruntjs/grunt-contrib-copy
+ * Usage              : $ npm install grunt-contrib-copy --save-dev
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________
+ */

48f0c9bc0e5e452f1fc84edc8a543fd3c7b14d3b
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 02:11:09 +0530

    Initial commit
    Path: config/tasks/less.js
    grunt-contrib-less: Compile LESS files to CSS.
    Usage: $ npm install grunt-contrib-less --save-dev

---
+++ config/tasks/less.js
@@ -0,0 +1,21 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * grunt-contrib-less : https://github.com/gruntjs/grunt-contrib-less
+ * Help               : http://lesscss.org/
+ * Usage              : $ npm install grunt-contrib-less --save-dev
+ * @require           : rules/.lessrc
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________
+ */

b4928fc690a83a0feeddf15e5431f93724dc245d
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-18 20:54:01 +0530

    Initial commit
    config.rb, is used to compile compass based framework, --/The Heart of Beauty/-- of "Apps®".
    Help: http://compass-style.org/.
    Usage: $ compass init

---
+++ config.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+
+# AXIX®
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://apps.sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Everest
+# @date       : 18-April-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle  : Standard Module Definition
+# -----------------------------------------

8400296cb54429373b5fd210fe9a93c5d0390adc
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 04:52:39 +0530

    Initial commit
    Path: config/tasks/replace.js
    grunt-string-replace: Replaces strings on files by using string or regex patterns.
    Usage: $ npm install grunt-string-replace --save-dev

---
+++ config/tasks/replace.js
@@ -0,0 +1,20 @@
+/*!
+ * App®
+ * _____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~
+ * grunt-string-replace : https://github.com/eruizdechavez/grunt-string-replace
+ * Usage                : $ npm install grunt-string-replace --save-dev
+ * _____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _____________________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * _____________________________________________________________________________
+ */

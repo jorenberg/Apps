bd5c2ead03e126d43f8fc2c9a90a8ce6573d2840
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-18 21:08:40 +0530

    Initial commit
    --/The Heart of Build System/-- of "Apps®".
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: Help: https://github.com/gruntjs or http://gruntjs.com/ and http://gruntjs.com/configuring-tasks
    Usage: $ grunt

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+
+/*!
+ * Apps® – [v1.0.0]
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 18-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Apps®".
+ * ______________________________________________________________
+ */

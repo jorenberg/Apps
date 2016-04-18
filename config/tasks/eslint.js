97bbbbf59642ba03bb8a114f28b55110a8cd866a
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 01:11:00 +0530

    Initial commit
    Path: config/tasks/eslint.js
    ESLint - A fully pluggable tool for identifying and reporting on patterns in JavaScript.
    Usage: $ npm install eslint -g

---
+++ config/tasks/eslint.js
@@ -0,0 +1,22 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * ESLint     : https://github.com/eslint/eslint
+ * Help       : http://eslint.org/
+ * Usage      : $ npm install eslint -g
+ * @require   : rules/.eslintrc
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________
+ */

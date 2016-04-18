552f67f642603232da98fb97cba6eedd1157bb41
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-18 21:15:50 +0530

    Initial commit
    --/The Heart of Test System/-- of "Apps®".
    karma, The Spectacular Test Runner for JavaScript.
    Help: https://github.com/karma-runner/karma
    Usage: $ karma init

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+
+/*!
+ * Apps® – [v1.0.0]
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 18-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "Apps®".
+ * ______________________________________________________________
+ */

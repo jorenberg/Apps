7519e114930303a4c3addc80814e025379cdb2cf
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 08:44:21 +0530

    Initial commit
    Path: config/tasks/utils.js
    Utility: Configure Grunt tasks to run with newer files only.
    Usage:
    1. $ npm install grunt-newer --save-dev
    2. $ npm install grunt-rsync --save-dev

---
+++ config/tasks/utils.js
@@ -0,0 +1,24 @@
+/*!
+ * Apps®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~
+ * Utility      : @require ================================================
+ *   |----------- Configure Grunt tasks to run with newer files only.
+ * grunt-newer  : https://github.com/tschaub/grunt-newer
+ * Usage        : $ npm install grunt-newer --save-dev                  [+]
+ * grunt-rsync  : https://github.com/jedrichards/grunt-rsync
+ * Usage        : $ npm install grunt-rsync --save-dev
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ________________________________________________________________________
+ */

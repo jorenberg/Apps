dafdf166f6ca231f3be5d3bfa06e64b4c9071b91
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 21:49:53 +0530

    Initial commit
    Reference Path: https://github.com/requirejs/r.js/blob/master/build/example.build.js

---
+++ config/tasks/amd/libs/example.build.js
@@ -0,0 +1,8 @@
+/*!
+ * This is an example build file that demonstrates -
+ * how to use the build system for /require.js/.
+ *
+ * THIS BUILD FILE WILL NOT WORK.
+ * It is referencing paths that probably
+ * Do not use on your machine. Just use it as a guide.
+ */

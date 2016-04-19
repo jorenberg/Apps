cc35c51207a739ec8712f17283b499648c348dfd
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 21:15:10 +0530

    Initial commit.💋
    Help:
    1. http://requirejs.org/docs/optimization.html
    2. https://github.com/amdjs/amdjs-api/wiki/AMD
    3. https://github.com/requirejs/r.js
    4. https://github.com/requirejs/r.js/blob/master/build/example.build.js

---
+++ config/tasks/amd/build.js
@@ -0,0 +1,30 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * AMD [Asychronous Module Defintion] : @require ==============================
+ * grunt-contrib-requirejs : https://github.com/gruntjs/grunt-contrib-requirejs
+ * help                    : https://github.com/requirejs/requirejs
+ * Usage                   : $ npm install grunt-contrib-requirejs --save-dev
+ *  |----------------------: from root // - Apps.config.js
+ * @require   :
+ * For Development Build   : --------------------------------------------------
+ * @using     :   r.js   // - https://github.com/requirejs/r.js
+ * @path      :     |-------- libs/r.js
+ * For Production Build    : --------------------------------------------------
+ * @using     :   almond // - https://github.com/requirejs/almond
+ * @path      :     |-------- libs/almond.js
+ * ____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________________
+ */

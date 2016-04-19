19e074599be3baf8b969fb3fd8bdb37c9948c791
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 21:30:00 +0530

    Initial commit
    Help:
    1. https://github.com/requirejs/r.js
    2. http://requirejs.org/docs/optimization.html

---
+++ config/tasks/amd/libs/r.js
@@ -0,0 +1,8 @@
+/*!
+ * r.js - [2.2.0]
+ * ~~~~~~~~~~~~~~
+ * Runs RequireJS in Node and Rhino, and used to run the RequireJS optimizer
+ * @author      : James Burke - http://jrburke.com/ | https://github.com/jrburke
+ * @copyright   : jQuery Foundation and other contributors.
+ * @license     : MIT license, http://github.com/requirejs/r.js/LICENSE
+ */

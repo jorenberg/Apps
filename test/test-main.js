62ad9813d81b882b827f5a36bae9699087752e77
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-20 02:44:50 +0530

    Initial commit
    Help:
    1. http://karma-runner.github.io/0.13/config/configuration-file.html
    2. http://karma-runner.github.io/0.13/plus/requirejs.html

---
+++ test/test-main.js
@@ -0,0 +1,16 @@
+/*!
+ * Apps®
+ * ~~~~~>     It configures Require.js for the tests.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 20-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : karma + grunt
+ * ______________________________________________________________
+ */

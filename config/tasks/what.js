2d4ddbbf307f365491c3993aac4902386edcaf3b
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 17:20:50 +0530

    Initial commit
    Path: config/tasks/what.js
    --> What-If settings to Compile HTML/SASS/LESS/Stylus/JavaScript through JSON configurations.

---
+++ config/tasks/what.js
@@ -0,0 +1,20 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~
+ * What If    : @require =====================================================
+ *   |-- What-If settings to Compile HTML/SASS/LESS/Stylus/JavaScript
+ * ____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________________
+ */

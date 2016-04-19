36a1bf0986d1f5b5718b3fddc6ce445cf456e5ff
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 21:36:56 +0530

    Initial commit
    Help: https://github.com/requirejs/almond

---
+++ config/tasks/amd/libs/almond.js
@@ -0,0 +1,8 @@
+/*!
+ * almond.js - [0.3.2]
+ * ~~~~~~~~~~~~~~~~~~~
+ * A minimal AMD API implementation for use after optimized builds
+ * @author      : James Burke - http://jrburke.com/ | https://github.com/jrburke
+ * @copyright   : jQuery Foundation and other contributors.
+ * @license     : MIT license, http://github.com/requirejs/almond/LICENSE
+ */

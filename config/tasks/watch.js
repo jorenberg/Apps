f4a8f64a34cfc20a2937bc8ce5fc565b6e464055
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 16:55:18 +0530

    Initial commit
    Path: config/tasks/watch.js
    Watch: Run predefined tasks whenever watched file patterns are added, changed or deleted.
    Usage: $ npm install grunt-contrib-watch --save-dev

---
+++ config/tasks/watch.js
@@ -0,0 +1,24 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * Watch               : @require =====================================
+ *   |------------------ HTML/SASS/LESS/Stylus/CSS/JavaScript/JSON
+ * grunt-contrib-watch : https://github.com/gruntjs/grunt-contrib-watch
+ * ____________________________________________________________________
+ * Usage               : $ npm install grunt-contrib-watch --save-dev
+ * Usage               : $ grunt serve
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________
+ */

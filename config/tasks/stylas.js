80fe0612f05367e76d54062146d7fe0cf9cfce5c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 06:59:49 +0530

    Initial commit
    Path: config/tasks/stylas.js
    grunt-contrib-stylus: Compile Stylus files to CSS
    Usage: $ npm install grunt-contrib-stylus --save-dev

---
+++ config/tasks/stylas.js
@@ -0,0 +1,28 @@
+/*!
+ * Apps®
+ * ______________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Stylus               : @require ======================================
+ * grunt-contrib-stylus : https://github.com/gruntjs/grunt-contrib-stylus
+ * Help                 : http://stylus-lang.com/
+ * Usage                : $ npm install grunt-contrib-stylus --save-dev
+ * !
+ * grunt-stylint        : https://github.com/xdissent/grunt-stylint
+ * Usage                : $ npm install grunt-stylint --save-dev
+ * @require             : rules/.stylintrc
+ * grunt-stylus-deps    : https://github.com/alright/grunt-stylus-deps
+ * Usage                : $ npm install grunt-stylus-deps --save-dev
+ * ______________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ______________________________________________________________________
+ */

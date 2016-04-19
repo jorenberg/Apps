80935e76edcb5bd20e793f0f9a85f71d35d7c860
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-19 17:04:18 +0530

    Initial commit
    Path: config/tasks/zipper.js
    Zipper: Grunt tasks to Compress files and folders.
    Usage: $ npm install grunt-contrib-compress --save-dev

---
+++ config/tasks/zipper.js
@@ -0,0 +1,22 @@
+/*!
+ * Apps®
+ * ____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Zipper       : @require ====================================================
+ *   |----------- Grunt tasks to Compress files and folders.
+ * grunt-contrib-compress : https://github.com/gruntjs/grunt-contrib-compress
+ * Usage                  : $ npm install grunt-contrib-compress --save-dev
+ * ____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________________
+ * @date      : 19-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt
+ * ____________________________________________________________________________
+ */

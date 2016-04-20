ff4c8b8d8c09d571a349761d201cb9b9d8f8c58b
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-20 18:59:51 +0530

    Initial commit
    boot.js, The Non-AMD JavaScript - for defining "index" configurations.
    Help: https://github.com/sequomics/Apps/wiki

---
+++ source/core/script/boot.js
@@ -0,0 +1,21 @@
+/*!
+ * Apps®: Applications & KnowledgeBases of Sequømics Corporation.
+ * _______________________________________________________________
+ * Apps®, https://github.com/sequomics/Apps
+ * The Non-AMD JavaScript - for defining "index" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * _______________________________________________________________
+ * @date      : 20-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "Apps®".
+ * _______________________________________________________________
+ */

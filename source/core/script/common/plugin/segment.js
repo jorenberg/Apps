49c739e172b9ed6612c3d518270b0a45a04af523
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-20 20:30:19 +0530

    Initial commit
    Segment®: A little JavaScript class (without dependencies) to draw and animate SVG path strokes.
    Help: https://github.com/lmgonzalves/segment

---
+++ source/core/script/common/plugin/segment.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~
+ * Segment®   : A little JavaScript class to draw and animate SVG path strokes.
+ * __________________________________________________________________________________
+ * @copyright : 2015, lmgonzalves, All Rights Reserved.
+ * @help      : https://github.com/lmgonzalves/segment
+ * @license   : MIT
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * __________________________________________________________________________________
+ */

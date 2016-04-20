65168c4c02166063dccbcb5cf8b54eb965afc522
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-20 10:18:19 +0530

    Initial commit
    compare.py, to compare fonts.

---
+++ source/core/fonts/compare.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__contributors__ = [
+    '"Emma Jörenberg" <https://github.com/jorenberg>',
+    '"Danielle Støvring" <https://github.com/stovring>'
+    ]
